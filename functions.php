281dc5797f21f02ef75404f49467a589dc717cf8
Jessica SantoSi <jessica@dgin.com.br>
2024-07-17 22:33:00 -0300

    atualizando functions.php

--- functions.php
+++ functions.php
@@ -17,11 +17,8 @@ define( 'CHILD_THEME_ASTRA_CHILD_VERSION', '1.0.0' );
  * Enqueue styles
  */
 function child_enqueue_styles() {
-
 	wp_enqueue_style( 'astra-child-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_ASTRA_CHILD_VERSION, 'all' );
-// Enfileirar o CSS do admin
-  wp_enqueue_style( 'dgin_admin_stylesheet', get_stylesheet_directory_uri() . '/style-admin.css', array(), CHILD_THEME_ASTRA_CHILD_VERSION, 'all' );
-
+	wp_enqueue_style( 'dgin_admin_stylesheet', get_stylesheet_directory_uri() . '/style-admin.css', array(), CHILD_THEME_ASTRA_CHILD_VERSION, 'all' );
 }
 
 add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
@@ -29,4 +26,3 @@ add_action( 'admin_enqueue_scripts', 'child_enqueue_styles' );
 
 // Inclui o arquivo automacoes-pgls-dgin.php
 require_once get_stylesheet_directory() . '/automacoes-pgls-dgin.php';
-
